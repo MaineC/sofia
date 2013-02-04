70b7cd327d672307a97a5660e81007f3d605760f
isabel <isabel@batou>
2013-02-04 21:01:49 +0100

    Add offset to indexing.

--- sof_es_poster.py
+++ sof_es_poster.py
@@ -19,27 +19,28 @@ counter = 1
 with open('../train-sample', 'rt') as csvfile:
   sofreader = csv.reader(csvfile, delimiter=',', quotechar='"')
   for row in sofreader:
-    [post_id, post_creation_date, owner_user_id, owner_creation_date, reputation_at_post_creation, owner_undeleted_answer_count_at_post_time, title, body, tag_1, tag_2, tag_3, tag_4, tag_5, post_closed_date, open_status] = row;
-    document = {
-        "post_id": post_id,
-        "post_creation_date": post_creation_date,
-        "owner_user_id": owner_user_id,
-        "owner_creation_date" : owner_creation_date,
-        "reputation_at_post_creation": reputation_at_post_creation,
-        "owner_undeleted_answer_count_at_post_time": owner_undeleted_answer_count_at_post_time,
-        "title": title,
-        "body": body,
-        "tag_1": tag_1,
-        "tag_2": tag_2,
-        "tag_3": tag_3,
-        "tag_4": tag_4,
-        "tag_5": tag_5,
-        "post_closed_date": post_closed_date,
-        "open_status": string.replace(open_status, " ", "_")}
-    print "Indexing"
-    print counter
-    print post_id
-    conn.index(document, "sof-sample", counter)
+    if (counter > 40000):
+        [post_id, post_creation_date, owner_user_id, owner_creation_date, reputation_at_post_creation, owner_undeleted_answer_count_at_post_time, title, body, tag_1, tag_2, tag_3, tag_4, tag_5, post_closed_date, open_status] = row;
+        document = {
+            "post_id": post_id,
+            "post_creation_date": post_creation_date,
+            "owner_user_id": owner_user_id,
+            "owner_creation_date" : owner_creation_date,
+            "reputation_at_post_creation": reputation_at_post_creation,
+            "owner_undeleted_answer_count_at_post_time": owner_undeleted_answer_count_at_post_time,
+            "title": title,
+            "body": body,
+            "tag_1": tag_1,
+            "tag_2": tag_2,
+            "tag_3": tag_3,
+            "tag_4": tag_4,
+            "tag_5": tag_5,
+            "post_closed_date": post_closed_date,
+            "open_status": string.replace(open_status, " ", "_")}
+        print "Indexing"
+        print counter
+        print post_id
+        conn.index(document, "sof-sample", counter)
     counter = counter + 1
 
 conn.indices.refresh(sof-sample)
