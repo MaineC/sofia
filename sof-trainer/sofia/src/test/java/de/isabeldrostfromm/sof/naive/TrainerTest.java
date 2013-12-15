919d5c83b25ddccc471b82f6583d7de73404c1cd
Isabel Drost-Fromm <isabel.drostfromm@elasticsearch.com>
2013-12-15 11:36:53 +0100

    Remaining refactoring in tests.

--- sof-trainer/sofia/src/test/java/de/isabeldrostfromm/sof/naive/TrainerTest.java
+++ sof-trainer/sofia/src/test/java/de/isabeldrostfromm/sof/naive/TrainerTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.isabeldrostfromm.sof.naive.mahout;
+package de.isabeldrostfromm.sof.naive;
 
 import java.io.IOException;
 import java.util.ArrayList;
